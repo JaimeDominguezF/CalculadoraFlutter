1b45584a76d64a959e577091b158dfbfa4999aa5
JaimeDominguezF <jdominguezforcadell@gmail.com>
2026-01-24 19:00:36 +0100

    Estructuracion del proyecto

---
+++ lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:calculator/app/routes.dart';
+import 'package:calculator/shared/theme/appTheme.dart';
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Apptheme.light,
+    );
+  }
+}
---
+++ lib/shared/theme/appTheme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Apptheme {
+
+  static final light = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+  );
+  
+  static final dark = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.orange,
+    scaffoldBackgroundColor: Colors.black,
+  );
+  
+}
